742cc61e8195c6e78ceca73770d37d2ca1754586
Rahul Srivastava <54400720+rahulsrivastava1@users.noreply.github.com>
2021-06-14 10:10:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer!", "Designer!","Competitive Programmer!","Thinker!", "Learner!"],
+        strings: ["Developer!", "Designer!", "Competitive Programmer!", "Thinker!", "Learner!"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
